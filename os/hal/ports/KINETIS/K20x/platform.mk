1499d4ac68f0167cae1b4cd3ab5992bb13a60d59
flabbergast <s3+flabbergast@sdfeu.org>
2015-09-20 20:11:40 +0100

    [KINETIS] Move st_lld into LLD.

--- os/hal/ports/KINETIS/K20x/platform.mk
+++ os/hal/ports/KINETIS/K20x/platform.mk
@@ -8,7 +8,7 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
               ${CHIBIOS}/os/hal/ports/KINETIS/LLD/ext_lld.c \
               ${CHIBIOS}/os/hal/ports/KINETIS/LLD/adc_lld.c \
               ${CHIBIOS}/os/hal/ports/KINETIS/K20x/gpt_lld.c \
-              ${CHIBIOS}/os/hal/ports/KINETIS/K20x/st_lld.c
+              ${CHIBIOS}/os/hal/ports/KINETIS/LLD/st_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
